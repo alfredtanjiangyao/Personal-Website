12934e987cb7a217209f89ce4565475ac2ce5ef3
alfredtanjiangyao <alfredtanjiangyao@gmail.com>
2024-12-20 22:54:39 +0800

    build file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://alfredtan.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/personal-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
